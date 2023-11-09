0f57d6473fb45e912fa6c5709d101b76cfc2f947
yhao <data4.cmi@unilever.com>
2023-11-09 14:05:56 +0800

    0程序正常退出

--- shell/receive_src_res/test.sh
+++ shell/receive_src_res/test.sh
@@ -1,12 +1,12 @@
 integer=$1
 python $(cd `dirname $0`; pwd)/test.py -i $integer
 # echo 'python脚本结果： ' $?  # 不能写，不然下一行的$?就变成了echo的系统返回值了
-if [ ! $? -eq 0 ]
+if [ $? -eq 0 ]
 then
     echo 'end'
 else
     echo 'interrupted'
 fi
 
-echo 'pid: ' | head -n 1 /home/yhao/code/learning/shell/test.py | xargs echo
-head -n 1 /home/yhao/code/learning/shell/test.py | 'pid: ' xargs echo
+echo 'pid: ' | head -n 1 /home/yhao/code/learning/shell/receive_src_res/test.py | xargs echo
+head -n 1 /home/yhao/code/learning/shell/receive_src_res/test.py | 'pid: ' xargs echo
