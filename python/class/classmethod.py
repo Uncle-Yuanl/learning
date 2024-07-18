f377ce9edfd968a104132c867c65d63f7a50c63e
yhao <data4.cmi@unilever.com>
2024-07-18 15:36:23 +0800

    自定义装饰器和classmethod的顺序

--- python/class/classmethod.py
+++ python/class/classmethod.py
@@ -3,6 +3,21 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
+from functools import wraps
+
+
+def retry(times):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            trytime = 0
+            while trytime < times:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    trytime += 1
+        return wrapper
+    return decorator
 
 
 class DataProcess:
@@ -19,6 +34,18 @@ class DataProcess:
     def infoi(self):
         self.logger.info('test')
 
+    @classmethod
+    @retry(5)
+    def func_need_retry(cls):
+        cls.logger.info('retry')
+
+    @retry(5)
+    @classmethod
+    def func_need_retry_rev(cls):
+        """这种方式不行，貌似都没执行
+        """
+        cls.logger.info('retry rev')
+    
 
 if __name__ == "__main__":
     DataProcess.info()
@@ -26,4 +53,5 @@ if __name__ == "__main__":
     d = DataProcess(logger)
     d.info()
     d.infoi()
-
+    DataProcess.func_need_retry()
+    DataProcess.func_need_retry_rev()
