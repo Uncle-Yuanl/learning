c6ed31e4ed3ab3ab63328854180b91191285a3f9
yhao <data4.cmi@unilever.com>
2025-04-08 17:10:35 +0800

    Agent 使用fastapi代理自定义结构符合openai格式

---
+++ AIGC/local/openai_api.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File   :   openai_api.py
+@Time   :   2025/04/08 14:56:20
+@Author :   yhao 
+@Email  :   data4.cmi@unilever.com
+@Desc   :   使用openai包调用自己封装的接口
+'''
+
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(f'【{__file__}】')
+import os
+from dotenv import load_dotenv, find_dotenv
+from openai import OpenAI, DefaultHttpxClient
+from openai import AsyncOpenAI, DefaultAsyncHttpxClient
+
+
+load_dotenv(find_dotenv(".env"), override=True)
+client = OpenAI(
+    api_key=os.getenv("TRANSFER_KEY"),  # 这个不能少，还不能为空
+    base_url="http://127.0.0.1:8000/v1/",
+    http_client=DefaultHttpxClient(
+        proxies="http://localhost:7890"
+    )
+)
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    ]
+)
+
+completion2 = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    ],
+    timeout=600,
+    max_tokens=4096,
+    temperature=0.0,
+    stream=True  # 加了就错
+)
+
+result = completion.choices[0].message.content
+
+print(result)
---
+++ AIGC/local/proxy.py
@@ -0,0 +1,98 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File   :   proxy.py
+@Time   :   2025/04/08 11:42:50
+@Author :   yhao 
+@Email  :   data4.cmi@unilever.com
+@Desc   :   当我们的接口不符合openai标准接口时，使用fastapi套壳转换
+'''
+
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(f'【{__file__}】')
+import os
+from typing import Optional, Dict
+from dotenv import find_dotenv, load_dotenv
+import json
+import requests
+import httpx, aiohttp
+from fastapi import FastAPI
+
+load_dotenv(find_dotenv(".env"), override=False)
+AZURE_TENANT_ID = os.getenv("AZURE_TENANT_ID")
+AZURE_CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
+AZURE_CLIENT_SECRET = os.getenv("AZURE_CLIENT_SECRET")
+AZURE_OPENAI_SUBSCRIPTION_KEY = os.getenv("AZURE_OPENAI_SUBSCRIPTION_KEY")
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World", "AZURE_TENANT_ID": AZURE_TENANT_ID}
+
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id}
+
+
+async def generate_access_token(client_id, client_secret):
+    auth_endpoint = f"https://login.microsoftonline.com/{AZURE_TENANT_ID}/oauth2/v2.0/token"
+    data = {
+        "grant_type": "client_credentials",
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "scope": "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default"
+    }
+    response = requests.post(auth_endpoint, data=data)
+
+    if response.ok:
+        access_token = response.json()["access_token"]
+        return access_token
+    else:
+        raise Exception(f"Authentication failed: {response.json()}")
+
+
+@app.post("/v1/chat/completions")
+async def get_completion(query: Dict, model="gpt-4o-mini"):
+    """Call the OpenAI API to get a completion for the given prompt."""
+    access_token = await generate_access_token(AZURE_CLIENT_ID, AZURE_CLIENT_SECRET)
+    if access_token is None:
+        return None
+    subs_key = AZURE_OPENAI_SUBSCRIPTION_KEY
+    
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Ocp-Apim-Subscription-Key" : subs_key,
+        "Content-Type": "application/json",
+        "User-Agent": ""  # 对aiohttp有效，httpx无效
+    }
+    env = 'prod' if AZURE_OPENAI_SUBSCRIPTION_KEY=='fa3bfc057f4946f09099ebc6214f564a' else 'uat'
+    unified_api = f"https://aiflmapi{env}.unilever.com/openai4/az_openai_{model}_chat"
+
+    # response = requests.post(
+    #     url=unified_api,
+    #     headers=headers,
+    #     data=json.dumps(query)
+    # )
+    # async with httpx.AsyncClient() as client:
+    #     response = await client.post(
+    #         url=unified_api,
+    #         headers={"Content-Type": "application/json", **headers},
+    #         data=json.dumps(query)
+    #     )
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.post(
+            url=unified_api,
+            data=json.dumps(query)
+        ) as response:
+            status = response.status
+            if status == 200:
+                return await response.json()
+            else:
+                raise Exception(f"Request failed: {response.status_code} {response.text}")
